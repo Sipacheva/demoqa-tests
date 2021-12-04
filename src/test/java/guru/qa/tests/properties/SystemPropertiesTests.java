ae08e21c149ca0e2cd82bf0495811edac5d764a9
Sipacheva <so.888@mail.ru>
2021-12-04 04:40:11 +0300

    added SystemPropertiesTests

---
+++ src/test/java/guru/qa/tests/properties/SystemPropertiesTests.java
@@ -0,0 +1,13 @@
+package guru.qa.tests.properties;
+
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+public class SystemPropertiesTests {
+  @Test
+  @Tag("properties")
+  void test4() {
+    String value = System.getProperty("browser", "chrome");
+    System.out.println(value);
+  }
+}
